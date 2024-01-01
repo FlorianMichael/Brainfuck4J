a6683bd92b2473f00866f8f0a075b9938ccd7c75
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-01-01 17:28:11 +0100

    2024

--- src/main/java/de/florianmichael/brainfuck4j/exception/BFRuntimeException.java
+++ src/main/java/de/florianmichael/brainfuck4j/exception/BFRuntimeException.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Brainfuck4J - https://github.com/FlorianMichael/Brainfuck4J
- * Copyright (C) 2023 FlorianMichael/EnZaXD and contributors
+ * Copyright (C) 2021-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
