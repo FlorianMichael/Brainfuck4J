781dc83da8725dfc2fb1559ca11c7edec1eee1d1
FlorianMichael <git@florianmichael.de>
2025-12-28 19:27:37 +0100

    Brainfuck4J 2.1.1: Prepare for next year (2026)

--- src/main/java/de/florianmichael/brainfuck4j/dialect/Dialect.java
+++ src/main/java/de/florianmichael/brainfuck4j/dialect/Dialect.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Brainfuck4J - https://github.com/FlorianMichael/Brainfuck4J
- * Copyright (C) 2021-2025 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
+ * Copyright (C) 2021-2026 FlorianMichael/EnZaXD <git@florianmichael.de> and contributors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
